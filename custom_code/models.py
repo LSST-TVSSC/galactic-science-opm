26b8c0993b993faa8bb3ae327182147587c1969c
mpgh <markus.hundertmark@uni-heidelberg.de>
2025-12-16 23:27:22 +0100

    rm unused class

--- custom_code/models.py
+++ custom_code/models.py
@@ -2,10 +2,3 @@
 
 #from tom_targets.base_models import BaseTarget
 
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    # upload_to='products/' means files will be stored in your_project/media/products/
-    image = models.ImageField(upload_to='products/')
-
-    def __str__(self):
-        return self.name
