35ad6379efd78264ec78d5553b3eabe2886b96a8
mpgh <markus.hundertmark@uni-heidelberg.de>
2025-12-17 21:51:45 +0100

    deserialize, prep tempdir and fit with RTModel

--- custom_code/management/commands/run_rtmodel_fits.py
+++ custom_code/management/commands/run_rtmodel_fits.py
@@ -1,14 +1,27 @@
 from django.core.management.base import BaseCommand
 from tom_dataproducts.models import ReducedDatum
 from tom_targets.models import Target,TargetExtra
+from django.conf import settings
 from django.db import transaction
 from astropy.time import Time
 from custom_code.target_models import GalacticTarget, MicrolensingModel, Classification
+from astropy.time import Time, TimezoneInfo
+from astropy.coordinates import SkyCoord, EarthLocation
+import RTModel
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import RTModel.plotmodel as plm
+import astropy.units as u
+import pandas as pd
+import tempfile
 import datetime
-import os
+from os import path
+from os import makedirs, listdir
 import numpy as np
 from django.db import connection
 
+    
 class Command(BaseCommand):
     help = 'Fit events with PSPL and parallax, then ingest fit parameters in MicrolensingModel'
 
@@ -18,20 +31,59 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         target = Target.objects.get(name=str(options['event']))
         target_id = target.id
-
-        with transaction.atomic():
-            photometry = ReducedDatum.objects.filter(data_type='photometry', target=target).order_by('-timestamp')
-            data = []
-            for reduced_datum in photometry:
-                rd_data = {'timestamp': reduced_datum.timestamp}
-                if 'limit' in reduced_datum.value.keys():
-                    rd_data['magnitude'] = reduced_datum.value['limit']
-                    rd_data['limit'] = True
+        tempdirname = 'event001'
+        print(target.name)
+        with tempfile.TemporaryDirectory() as tempdirname:      
+            data_dir = path.join(tempdirname,'Data')
+            makedirs(data_dir)
+            input_path = path.join(tempdirname, 'input_data.csv')
+            model_output = path.join(tempdirname, 'model_results.pkl')
+            #RTModel.fit(input_path, output=model_output)
+            with transaction.atomic():
+                photometry = ReducedDatum.objects.filter(data_type='photometry', target=target).order_by('-timestamp')
+                data = []
+                #required format # Mag err HJD-2450000
+                target_coord = SkyCoord(ra = target.ra, dec = target.dec, unit='deg', frame='icrs')
+                # RTM requires a sexagesimal string (HH:MM:SS.S DD:MM:SS.S)
+                with open(path.join(data_dir,"event001.coordinates"),'w') as fp:
+                    fp.write(target_coord.to_string('hmsdms',sep=':'))
+                
+                if "ZTF" in str(target.name):
+                    location = EarthLocation.from_geodetic(lon=-116.859722 * u.deg, lat=33.357222 * u.deg,
+                                                           height=1700 * u.m)   
                 else:
+                    #default location for all other data, Rubin
+                    location = EarthLocation.from_geodetic( lat='-30d14m40.68s', lon='-70d44m57.90s', height=2647.*u.m)
+                for reduced_datum in photometry:
+                    t = Time(reduced_datum.timestamp, scale='utc')
+                     
+                    ltt_heliocentric = t.light_travel_time(target_coord, kind='heliocentric', location=location)
+                    hjd_values_rtm = t.jd + ltt_heliocentric.value -2450000.
+                    rd_data = {'timestamp': hjd_values_rtm }
+
                     rd_data['magnitude'] = reduced_datum.value['magnitude']
-                    rd_data['limit'] = False
-                data.append(rd_data)
-            print(data)
+                    rd_data['error'] = reduced_datum.value['error']
+                    rd_data['filter'] = reduced_datum.value['filter']
+                    data.append(rd_data)
+                df = pd.DataFrame.from_dict(data)
+                unique_categories = df['filter'].unique()
+                for category in unique_categories:
+                    filtered_df = df[df['filter'] == category]
+                    filtered_df.to_csv(path.join(data_dir,f"{category}.dat"), columns= ['magnitude','error','timestamp'], 
+                              header=None, index=None, sep=' ', mode='a')
+                rtm = RTModel.RTModel(tempdirname)
+                rtm.config_InitCond(modelcategories = ['PS'])
+                rtm.run()
+                event_path = path.join(tempdirname)
+                saving_path =path.join(settings.MEDIA_ROOT, f"{target.name}.png")
+                model_name = listdir(path.join(tempdirname,"FinalModels"))
+
+                model_path = path.join(tempdirname,"FinalModels",model_name[0])
+                plm.plotmodel(eventname=event_path, modelfile=model_path)
+                print(listdir(data_dir))
+                plt.savefig(saving_path, bbox_inches='tight')
+    
+                
 
 if __name__ == '__main__':
     main()
--- custom_code/views.py
+++ custom_code/views.py
@@ -8,14 +8,15 @@ from django.shortcuts import render, get_object_or_404
 from os import path
 
 def microlensing_model_view(request):
-    microlensing_models = MicrolensingModel.objects.all()  # Get all MicrolensingModels
+    microlensing_models = MicrolensingModel.objects.all()[:30]  # Get 30 MicrolensingModels
     try:
         return render(request, 'custom_code/model_list.html', {'microlensing_models': microlensing_models})
     except ObjectDoesNotExist:
         return render(request, 'custom_code/model_list.html', {'microlensing_models': microlensing_models})
     
 def microlensing_prob_view(request):
-    microlensing_objects = Classification.objects.order_by("-prob_class1")
+    #so far prob class 1 is microlensing, but the model permits other options, to be filtered.
+    microlensing_objects = Classification.objects.order_by("-prob_class1")[:30]
     try:
         return render(request, 'custom_code/prob_list.html', {'microlensing_objects': microlensing_objects})
     except ObjectDoesNotExist:
@@ -32,7 +33,7 @@ class HomeView(TemplateView):
         # (we know they at least have id and name).
         # This can be modifed with any selector function later.
         # First modification: prob_class1
-        target_ids = Classification.objects.all().order_by("-prob_class1").values_list('target_id', flat=True).distinct()[:11]
+        target_ids = Classification.objects.all().order_by("-prob_class1").values_list('target_id', flat=True).distinct()[:8]
         target_objects = GalacticTarget.objects.filter(id__in=target_ids)
         featured = (target_objects)
         context["featured_targets"] = featured
