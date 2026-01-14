23ecfa7ef552aeffeb3560344aed3e7fd4f55b1d
mpgh <markus.hundertmark@uni-heidelberg.de>
2026-01-14 01:06:40 +0100

    ingest bogus stamp classifier ALeRCE

--- custom_code/management/commands/ingest_alerce_ztf_probabilities.py
+++ custom_code/management/commands/ingest_alerce_ztf_probabilities.py
@@ -23,17 +23,22 @@ class Command(BaseCommand):
             time_now = Time(datetime.datetime.now()).jd
             alerce = Alerce()
             probabilities = alerce.query_probabilities(target.name)
-
+            
             prob_pd = pd.DataFrame.from_dict(probabilities)
             stochastic_bhrf_prob = prob_pd.loc[prob_pd['classifier_name'] == 'lc_classifier_BHRF_forced_phot']
             prob_class1 = float(stochastic_bhrf_prob[stochastic_bhrf_prob['class_name'] == 'Microlensing']['probability'].iloc[0])
             prob_class2 = float(stochastic_bhrf_prob[stochastic_bhrf_prob['class_name'] == 'CV/Nova']['probability'].iloc[0])
-            
+        
+            bogus_prob = prob_pd.loc[prob_pd['classifier_name'] == 'stamp_classifier']
+            prob_class3 = float(bogus_prob[bogus_prob['class_name'] == 'bogus']['probability'].iloc[0])
             m = Classification.objects.update_or_create(target=target,
                                               source='ALeRCE_ZTF',
                                               class1='microlensing',
                                               prob_class1 = prob_class1,
                                               class2='cv/nova',
-                                              prob_class2 = prob_class2)
+                                              prob_class2 = prob_class2,
+                                              class3='bogus',
+                                              prob_class3 = prob_class3
+                                              )
             
         print('probabilities created/updated.')
