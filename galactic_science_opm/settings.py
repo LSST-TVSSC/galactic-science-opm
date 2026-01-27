810528ffcaab55c294d30c395b98df0a7920d810
mpgh <markus.hundertmark@uni-heidelberg.de>
2026-01-27 10:07:01 +0100

    fix typo in settings

--- galactic_science_opm/settings.py
+++ galactic_science_opm/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default='7jj1ni6=mw867=jwr1(cr64()cd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG',default=False)
 
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS',default=['127.0.01','localhost'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS',default=['127.0.0.1','localhost'])
 CSRF_TRUSTED_ORIGINS = env.list('DJANGO_CSRF_TRUSTED_ORIGIN',default=[])
 
 
