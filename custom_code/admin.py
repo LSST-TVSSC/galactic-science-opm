3470fb9002b310d773eafa8acba91ad2c495b14a
Max Kistner <max.kistner@uni-heidelberg.de>
2026-06-23 11:27:41 +0200

    Add new entities to admin view

--- custom_code/admin.py
+++ custom_code/admin.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
-from .target_models import MicrolensingModel
+from .target_models import ClassificationGeneralized, ClassificationSource, MicrolensingModel
 from .target_models import MicrolensingRadarData
 from .target_models import Classification
 
 admin.site.register(MicrolensingModel)
 admin.site.register(MicrolensingRadarData)
 admin.site.register(Classification)
+admin.site.register(ClassificationGeneralized)
+admin.site.register(ClassificationSource)
