948cb84e8e759b61c11a25849b486edc534dc099
Max Kistner <max.kistner@uni-heidelberg.de>
2026-07-13 16:53:56 +0200

    WIP: Update conversion script for ReducedDatum to PhotometryReducedDatum

--- custom_code/management/commands/convert_reduceddatum_to_photometryreduceddatum.py
+++ custom_code/management/commands/convert_reduceddatum_to_photometryreduceddatum.py
@@ -3,15 +3,15 @@ from django.db import IntegrityError, transaction
 from tom_dataproducts.models import PhotometryReducedDatum, ReducedDatum
 
 
-class Command(BaseCommand):
-    help = "Convert existing photometry ReducedDatums to PhotometryReducedDatum"
+def convert_all_reduceddatum_to_photometryreduceddatum():
+    BATCH_SIZE = 2500
+    conversion_candiates = list()
+    creation_counter = 0
+    reduceddatums_for_target = ReducedDatum.objects.iterator(chunk_size=BATCH_SIZE)
+    number_of_reduceddatums = ReducedDatum.objects.count()
 
-    def add_arguments(self, parser):
-        parser.add_argument("target_names", nargs="*", type=str)
-
-    def handle(self, *args, **options):
-        reduceddatums_for_target = ReducedDatum.objects.all()
-        for reduceddatum in reduceddatums_for_target:
+    for reduceddatum in reduceddatums_for_target:
+        try:
             values = reduceddatum.value
             brightness = values["magnitude"]
             brightness_error = values["error"]
@@ -21,53 +21,49 @@ class Command(BaseCommand):
             exposure_time = None
             timestamp = reduceddatum.timestamp
             target = reduceddatum.target
-            try:
-                with transaction.atomic():
-                    _, created = PhotometryReducedDatum.objects.update_or_create(
-                        brightness=brightness,
-                        brightness_error=brightness_error,
-                        limit=limit,
-                        unit=unit,
-                        bandpass=bandpass,
-                        exposure_time=exposure_time,
-                        timestamp=timestamp,
-                        target=target,
-                        source_name=reduceddatum.source_name,
-                        source_location=reduceddatum.source_location,
-                    )
-                    if created:
-                        self.stdout.write(
-                            self.style.SUCCESS(
-                                f"Successfully converted ReducedDatum with pk {reduceddatum.id}"
-                            )
-                        )
-                    else:
-                        self.stdout.write(
-                            self.style.SUCCESS(
-                                f"Successfully updated PhotometryReducedDatum with pk {reduceddatum.id}"
-                            )
-                        )
 
-            except IntegrityError as e:
-                if "duplicate key" in str(e):
-                    self.stdout.write(
-                        self.style.NOTICE(
-                            f"Skipping ReducedDatum with pk {reduceddatum.id} as it already exists"
-                        )
-                    )
-                    continue
-                else:
-                    print(
-                        f"There was a problem migrating PhotometryReducedDatum for pk: {reduceddatum.id}"
-                    )
-                    print(e.__class__.__name__)
-                    print(e)
-                    continue
+            convertee = PhotometryReducedDatum(
+                brightness=brightness,
+                brightness_error=brightness_error,
+                limit=limit,
+                unit=unit,
+                bandpass=bandpass,
+                exposure_time=exposure_time,
+                timestamp=timestamp,
+                target=target,
+                source_name=reduceddatum.source_name,
+                source_location=reduceddatum.source_location,
+            )
+            conversion_candiates.append(convertee)
 
-            except Exception as e:
-                print(
-                    f"There was a problem migrating PhotometryReducedDatum for pk: {reduceddatum.id}"
-                )
-                print(e.__class__.__name__)
-                print(e)
-                continue
+            if len(conversion_candiates) == BATCH_SIZE:
+                created = PhotometryReducedDatum.objects.bulk_create(conversion_candiates)
+                creation_counter += len(created)
+                conversion_candiates.clear()
+                print(f"Created {creation_counter} of {number_of_reduceddatums}...")
+
+        except Exception as e:
+            print(f"could not convert reduceddatum with pk {reduceddatum.id}")
+            print(e.__class__.__name__)
+            print(e)
+
+    # insert remaining ones
+    if conversion_candiates:
+        created = PhotometryReducedDatum.objects.bulk_create(conversion_candiates)
+        creation_counter += len(created)
+        print(f"Created {creation_counter} of {number_of_reduceddatums}...")
+
+    print(
+        f"Converted {creation_counter} of {number_of_reduceddatums} ReducedDatums"
+        f" to PhotometryReducedDatum"
+    )
+
+
+class Command(BaseCommand):
+    help = "Convert existing photometry ReducedDatums to PhotometryReducedDatum"
+
+    def add_arguments(self, parser):
+        parser.add_argument("target_names", nargs="*", type=str)
+
+    def handle(self, *args, **options):
+        convert_all_reduceddatum_to_photometryreduceddatum()
