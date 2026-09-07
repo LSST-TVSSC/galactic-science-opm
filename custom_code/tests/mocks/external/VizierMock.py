78720b5c6aeb8f980c01cadffa8fcfe76006320a
Max Kistner <max.kistner@uni-heidelberg.de>
2026-09-07 14:53:35 +0200

    Change path handling in VizierMock

--- custom_code/tests/mocks/external/VizierMock.py
+++ custom_code/tests/mocks/external/VizierMock.py
@@ -1,12 +1,14 @@
+from os import path
 import pickle
 
 
 class VizierMock:
     def query_region(self, *args, **kwargs):
-        VIZIER_QUERY_REGION_PICKLE = (
-            "custom_code/tests/mocks/external/tablelist_for_Vizier.query_region_ZTF21abasvhl.pkl"
+        data_folder = path.join("custom_code", "tests", "mocks", "external")
+        VIZIER_QUERY_REGION_PICKLE = path.join(
+            data_folder,
+            "tablelist_for_Vizier.query_region_ZTF21abasvhl.pkl",
         )
         with open(VIZIER_QUERY_REGION_PICKLE, "rb") as f:
             result = pickle.load(f)
         return result
-
