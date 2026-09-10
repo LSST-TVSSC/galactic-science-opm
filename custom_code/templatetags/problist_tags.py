342c816f440c9f1fe0870d3ef87e45300fb3da48
mpgh <markus.hundertmark@uni-heidelberg.de>
2026-09-10 18:35:07 +0200

    add active tag to list, subgroup request

--- custom_code/templatetags/problist_tags.py
+++ custom_code/templatetags/problist_tags.py
@@ -1,12 +1,14 @@
 from django import template
 from django.utils.safestring import mark_safe
+from astropy.time import Time
+from custom_code.target_models import MicrolensingParameterModel
 
 register = template.Library()
 
 
 def make_target_tags_list(target):
     tags = list()
-    DAYS_CUTOFF = 3
+    DAYS_CUTOFF = 5
 
     if target["age_days"] < DAYS_CUTOFF:
         tags.append({
@@ -14,12 +16,30 @@ def make_target_tags_list(target):
             "text": f"Created less than {DAYS_CUTOFF} days ago"
         })
 
-    if target["object"].metric_bogus > 0.5:
+    if target["object"].metric_bogus > 0.6:
         tags.append({
             "class": "bogus",
-            "text": "bogus metric > 0.5"
+            "text": "bogus metric > 0.6"
         })
+    target_radar = target["object"].target  
+    params = (MicrolensingParameterModel.objects
+          .filter(target=target_radar)
+          .order_by("-updated_at")
+          .only("t0", "tE")
+          .first())
 
+    t0 = tE = None
+    if params is not None:
+        if params.t0 is not None:
+            t0 = params.t0 + 2450000.
+        tE = params.tE
+    if t0 is not None and tE is not None:
+        try:
+            current_jd = Time.now().jd
+            if float(t0) - float(tE) < current_jd < float(t0) + float(tE):
+                tags.append({"class": "active", "text": "Microlensing event current t in [t0-tE,t0+tE]"})
+        except (ValueError, TypeError):
+            pass
 
     return tags
 
@@ -38,9 +58,11 @@ def target_tags_elements(target):
     for tag in tags:
         if tag["class"] in bad_tags:
             elements.append(f"<div title='{tag['text']}' class='target-tag bad'>{tag['class']}</div>")
+        elif tag["class"] == "active":
+            elements.append(f"<div title='{tag['text']}' class='target-tag' style='background-color: orange; color: white; border-color: orange;'>{tag['class']}</div>")
         else:
             elements.append(f"<div title='{tag['text']}' class='target-tag' >{tag['class']}</div>")
 
+
+
     return mark_safe("".join(elements))
-
-
